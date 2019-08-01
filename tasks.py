9a77d56c491f10a4ffac875b0a064dcc88d8ea84
fvianamo <filipevianam@gmail.com>
2019-08-01 18:47:42 -0300

    documentando as tasks

--- tasks.py
+++ tasks.py
@@ -1,9 +1,12 @@
 from celery import Celery
 
+#defini string de conexão com redis
 BROKER_URL = "redis://" + "253b5d509e87"
 
+#instancia objeto Celery com bocker e backend
 app = Celery('tasks', backend=BROKER_URL ,broker=BROKER_URL)
 
+#defini função para teste
 @app.task
 def add(x,y):
 	return x + y
